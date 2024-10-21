2b05e19bb26413693a883139c08695fe593ad741
raphacmartin <raphaelcastromartin@gmail.com>
2024-10-21 11:28:50 -0300

    Created Hello World project

---
+++ ArcMoney/ArcMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArcMoneyApp.swift
+//  ArcMoney
+//
+//  Created by Raphael Martin on 21/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ArcMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
