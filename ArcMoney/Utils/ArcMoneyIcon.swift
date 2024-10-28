3cdfc11584da7b8a886137805d4dc97bc13166d7
Matheus Vaccaro <matheus.vaccaro@arctouch.com>
2024-10-28 12:49:24 -0300

    Add ArcMoneyIcon enum

---
+++ ArcMoney/Utils/ArcMoneyIcon.swift
@@ -0,0 +1,24 @@
+//
+//  ArcMoneyIcon.swift
+//  ArcMoney
+//
+//  Created by Matheus Vaccaro on 10/28/24.
+//
+
+enum ArcMoneyIcon: String {
+    case shopping = "cart.fill"
+    case food = "fork-knife"
+    case entertainment = "popcorn.fill"
+    case transportation = "bus"
+    case education = "graduationcap.fill"
+    case healthcare = "stethoscope"
+    case salary = "dollarsign"
+    case investment = "chart.line.uptrend.xyaxis"
+    case notification = "bell"
+    case goBack = "arrow.left"
+    case homeUnselected = "house"
+    case homeSelected = "house.fill"
+    case settingsUnselected = "gearshape"
+    case settingsSelected = "gearshape.fill"
+    case dropdownArrow = "chevron.down"
+}
