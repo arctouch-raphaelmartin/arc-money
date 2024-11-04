5ca60b787b918a39a8ac410ba8c1db705d1b91c7
Matheus Vaccaro <matheus.vaccaro@arctouch.com>
2024-11-04 17:29:45 -0300

    Add ArcMoneySize

---
+++ ArcMoney/Utils/ArcMoneySize.swift
@@ -0,0 +1,19 @@
+//
+//  ArcMoneySize.swift
+//  ArcMoney
+//
+//  Created by Matheus Vaccaro on 10/30/24.
+//
+
+import Foundation
+
+// MARK: - ArcMoneySize
+
+enum ArcMoneySize: CGFloat {
+    
+    // MARK: Cases
+    
+    case small = 18
+    case medium = 24
+    case large = 38
+}
