ce77879224fcc1d32a6c15208f644d91b0e0b352
Jamile Bastos <120420726+arctouch-jamilebastos@users.noreply.github.com>
2024-11-21 10:42:40 -0300

    Add colors to assets catalog (#10)
    * 🎨 adding colors to assets file
    
    * change the appearences field to None
    
    * Add app colors
    
    * refactor: rename enum for colors and delete folder
    
    * Remove extra line
    
    * refactor on colors
    
    * refactor on colors

---
+++ ArcMoney/Utils/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  ArcMoney
+//
+//  Created by Jamile Bastos on 25/10/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let gray = Color("Gray")
+    static let primary = Color("Primary")
+    static let secondary = Color("Secondary")
+}
