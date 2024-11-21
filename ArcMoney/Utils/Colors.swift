7d20b409108e45c7389e07b2cdf4928bc6483cd0
Jamile <jamile.bastos@arctouch.com>
2024-11-21 10:05:31 -0300

    refactor on colors

--- ArcMoney/Utils/Colors.swift
+++ ArcMoney/Utils/Colors.swift
@@ -7,11 +7,6 @@
 
 import SwiftUI
 
-//    static let backdrop = Color("Backdrop")
-//    static let dark = Color("Dark")
-//    static let light = Color("Light")
-
-// SwiftUI automatically creates static properties for some colors from the XCAssets. Adding here only the custom ones
 extension Color {
     static let gray = Color("Gray")
     static let primary = Color("Primary")
