a6640f8a109d295acb749d24d0a6baf873b6895d
Matheus Vaccaro <matheus.vaccaro@arctouch.com>
2024-11-26 00:18:45 -0300

    Add ArcMoneySpacing

---
+++ ArcMoney/Utils/ArcMoneySpacing.swift
@@ -0,0 +1,26 @@
+//
+//  ArcMoneySpacing.swift
+//  ArcMoney
+//
+//  Created by Matheus Vaccaro on 11/26/24.
+//
+
+import Foundation
+
+// MARK: - ArcMoneySpacing
+
+enum ArcMoneySpacing: CGFloat {
+    
+    // MARK: Cases
+    
+    case quarter = 4
+    case half = 8
+    case threeQuarters = 12
+    case one = 16
+    case oneAndQuarter = 20
+    case oneAndHalf = 24
+    case oneAndThreeQuarters = 28
+    case two = 32
+    case three = 48
+    case four = 64
+}
