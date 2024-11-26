9cd8ae6b10871deeb3d9db09a97b29d639aca92b
Matheus Vaccaro <matheus.vaccaro@arctouch.com>
2024-11-26 01:03:12 -0300

    Add documentation for ArcMoneySpacing cases

--- ArcMoney/Utils/ArcMoneySpacing.swift
+++ ArcMoney/Utils/ArcMoneySpacing.swift
@@ -13,14 +13,24 @@ enum ArcMoneySpacing: CGFloat {
     
     // MARK: Cases
     
+    /// 4 pts of spacing.
     case quarter = 4
+    /// 8 pts of spacing.
     case half = 8
+    /// 12 pts of spacing.
     case threeQuarters = 12
+    /// 16 pts of spacing.
     case one = 16
+    /// 20 pts of spacing.
     case oneAndQuarter = 20
+    /// 24 pts of spacing.
     case oneAndHalf = 24
+    /// 28 pts of spacing.
     case oneAndThreeQuarters = 28
+    /// 32 pts of spacing.
     case two = 32
+    /// 48 pts of spacing.
     case three = 48
+    /// 64 pts of spacing.
     case four = 64
 }
