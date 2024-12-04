5929f8d5a8b34d3b48edd0c0440693ceac1a49ad
Matheus Vaccaro <166859405+arctouch-matheusvaccaro@users.noreply.github.com>
2024-12-04 12:42:04 -0300

    [Issue-3] Create Icon Component (#13)
    * Add ArcMoneyIcon enum
    
    * Add MARKs and image computed property to ArcMoneyIcon
    
    * Add ArcMoneySize
    
    * Add IconView
    
    * Add more color examples for IconView

---
+++ ArcMoney/Utils/ArcMoneyIcon.swift
@@ -0,0 +1,37 @@
+//
+//  ArcMoneyIcon.swift
+//  ArcMoney
+//
+//  Created by Matheus Vaccaro on 10/28/24.
+//
+
+import SwiftUI
+
+// MARK: - ArcMoneyIcon
+
+enum ArcMoneyIcon: String {
+    
+    // MARK: Cases
+    
+    case shopping = "cart.fill"
+    case food = "fork-knife"
+    case entertainment = "popcorn.fill"
+    case transportation = "bus"
+    case education = "graduationcap.fill"
+    case healthcare = "stethoscope"
+    case salary = "dollarsign"
+    case investment = "chart.line.uptrend.xyaxis"
+    case notification = "bell"
+    case goBack = "arrow.left"
+    case homeUnselected = "house"
+    case homeSelected = "house.fill"
+    case settingsUnselected = "gearshape"
+    case settingsSelected = "gearshape.fill"
+    case dropdownArrow = "chevron.down"
+    
+    // MARK: Internal Properties
+    
+    var image: Image {
+        Image(systemName: self.rawValue)
+    }
+}
