9b91e2f3f4435962435905999d145fadb6dba9c4
Matheus Vaccaro <matheus.vaccaro@arctouch.com>
2025-02-12 14:10:40 -0300

    Add Currency enum with formatter

---
+++ ArcMoney/Design System/Utils/Currency.swift
@@ -0,0 +1,37 @@
+import Foundation
+
+// MARK: - Currency
+
+enum Currency: String {
+    
+    // MARK: Cases
+    
+    case brazilianReal = "Brazilian Real"
+    case unitedStatesDollar = "United Stated Dollar"
+    
+    // MARK: Internal Properties
+    
+    var symbol: String {
+        switch self {
+        case .brazilianReal:
+            "R$"
+        case .unitedStatesDollar:
+            "$"
+        }
+    }
+    
+    var isoCode: String {
+        switch self {
+        case .brazilianReal:
+            "BRL"
+        case .unitedStatesDollar:
+            "USD"
+        }
+    }
+    
+    // MARK: Internal Methods
+    
+    func formatWithSymbol(_ value: Double, floatingPointNumber: Int = 2) -> String {
+        return value.formatted(.currency(code: self.isoCode))
+    }
+}
